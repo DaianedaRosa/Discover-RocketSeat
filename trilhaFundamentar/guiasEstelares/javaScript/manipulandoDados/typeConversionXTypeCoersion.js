4a0287690b41d612dc0a7251da974788e91a152b
Daiane <daianedarosadev@gmail.com>
2023-02-13 20:33:18 -0400

    ✅ module 'manipulating data' completed

---
+++ trilhaFundamentar/guiasEstelares/javaScript/manipulandoDados/typeConversionXTypeCoersion.js
@@ -0,0 +1,9 @@
+// Type conversion (typecasting) vs Type Coersion;
+
+console.log('10' + 5) // coersion, transforma o 5 de number pra string, assim concatenando quando chamado.
+
+
+
+// Alteração de um tipo de dado para outro tipo, conversion;
+
+console.log(Number('10') + 5)
